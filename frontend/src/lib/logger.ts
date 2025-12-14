fe6934f8dec71c558792c9541e8462b083261a34
faridlia171-ship-it <faridlia171@hotmail.com>
2025-12-14 04:21:15 +0100

    Add missing logger.ts required by apiClient

---
+++ frontend/src/lib/logger.ts
@@ -0,0 +1,14 @@
+﻿// Simple logger compatible Next.js / Browser / Server
+const logger = {
+  info: (...args: any[]) => {
+    if (typeof console !== "undefined") console.info("[INFO]", ...args);
+  },
+  error: (...args: any[]) => {
+    if (typeof console !== "undefined") console.error("[ERROR]", ...args);
+  },
+  warn: (...args: any[]) => {
+    if (typeof console !== "undefined") console.warn("[WARN]", ...args);
+  },
+};
+
+export default logger;
